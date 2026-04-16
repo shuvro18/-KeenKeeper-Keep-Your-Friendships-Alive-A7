9f99004289b424b2a6c68d4b16085fcf6c740fe7
shuvro18 <shuvrobiswas404@gmail.com>
2026-04-17 01:18:54 +0600

    navbar added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import logoXl from "../../assets/logo-xl.png";
-import instagram from "../../assets/instagram.png";
-import facebook from "../../assets/facebook.png";
-import X from "../../assets/twitter.png"
+
 
 const Footer = () => {
     return (
-        
+        ""
     );
 };
 
--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -8,7 +8,30 @@ import { GoGraph } from 'react-icons/go';
 const NavBar = () => {
     return (
         <div className=' navbar bg-base-100 shadow-sm'>
-           
+            <div className=" w-11/12 mx-auto flex justify-between">
+                <div>
+                    <NavLink to={"/"}>
+                        <img src={logo} alt="logo" />
+                    </NavLink>
+                </div>
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal px-1">
+                        <NavLink to={"/"} className={({ isActive }) => `flex justify-between items-center mr-2 gap-1 btn ${isActive ? ("bg-[#244D3F] text-white") : "text-neutral/50"}`}>
+                            <FaHome />
+                            <p>Home</p>
+                        </NavLink>
+                        <NavLink to={"/timeline"} className={({ isActive }) => `flex justify-between items-center mr-2 gap-1 btn ${isActive ? ("bg-[#244D3F] text-white") : "text-neutral/50"}`}>
+                            <IoMdTime />
+                            <p>Timeline</p>
+                        </NavLink>
+                        <NavLink to={"/state"} className={({ isActive }) => `flex justify-between items-center mr-2 gap-1 btn ${isActive ? ("bg-[#244D3F] text-white") : "text-neutral/50"}`}>
+                            <GoGraph />
+                            <p>State</p>
+                        </NavLink>
+
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
