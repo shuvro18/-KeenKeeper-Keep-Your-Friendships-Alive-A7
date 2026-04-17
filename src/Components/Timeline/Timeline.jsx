038fa48d6b00ac57b1ee181bc4c94237b151dc39
shuvro18 <shuvrobiswas404@gmail.com>
2026-04-17 16:15:18 +0600

    filtering by call,video call, text, added

--- src/Components/Timeline/Timeline.jsx
+++ src/Components/Timeline/Timeline.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { friendContext } from '../FriendContext/Context';
 import ShowFriendsTimeline from '../ShowFriendsTimeline/ShowFriendsTimeline';
 
@@ -6,6 +6,8 @@ const Timeline = () => {
 
     const { button } = useContext(friendContext);
 
+    const [makeShort, setMakeShort] = useState("");
+    const displayItems = makeShort === ""? button : button.filter(item => item.action === makeShort)
 
 
 
@@ -17,15 +19,15 @@ const Timeline = () => {
                     <div className="dropdown  dropdown-start">
                         <div tabIndex={0} role="button" className="btn m-1">Filter Timeline  ⬇️</div>
                         <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
-                            <li onClick={()=>} ><a>Show All Calls</a></li>
-                            <li><a>Show All Text</a></li>
-                            <li><a>Show All Video(call)</a></li>
+                            <li onClick={() => setMakeShort("call")} ><a>Show All Calls</a></li>
+                            <li onClick={() => setMakeShort("text")}><a>Show All Text</a></li>
+                            <li onClick={() => setMakeShort("video")}><a>Show All Video(call)</a></li>
                         </ul>
                     </div>
                 </div>
                 <div className='flex flex-col gap-5'>
                     {
-                        button.map((element, index) => <ShowFriendsTimeline key={index} element={element}></ShowFriendsTimeline>)
+                        displayItems.map((element, index) => <ShowFriendsTimeline key={index} element={element}></ShowFriendsTimeline>)
                     }
                 </div>
             </div>
