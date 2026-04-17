c7c9cf3077aa34443a6c30be2cac524abac5164b
shuvro18 <shuvrobiswas404@gmail.com>
2026-04-17 20:54:31 +0600

    pie recharts section made

--- src/Components/State/State.jsx
+++ src/Components/State/State.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { friendContext } from '../FriendContext/Context';
+import { Pie, PieChart, Tooltip } from 'recharts';
 
 const State = () => {
+    const { button } = useContext(friendContext);
+
+    const callCount = button.filter(item => item.action === "call").length;
+    const textCount = button.filter(item => item.action === "text").length;
+    const videoCount = button.filter(item => item.action === "video").length;
+    
+
+
+    const data = [
+        { name: 'Calls', value: callCount, fill: '#0088FE' },
+        { name: 'Text', value: textCount, fill: '#00C49F' },
+        { name: 'Video', value: videoCount, fill: '#FFBB28' },
+
+    ];
+
     return (
-        <div>
-            <h2>This one is State</h2>
+        <div className='bg-base-200'>
+            <div className='flex justify-center items-center py-20'>
+
+                <PieChart style={{ width: '100%', maxWidth: '500px', maxHeight: '80vh', aspectRatio: 1 }} responsive>
+                    <Pie
+                        data={data}
+                        innerRadius="80%"
+                        outerRadius="100%"
+                        // Corner radius is the rounded edge of each pie slice
+                        cornerRadius="50%"
+                        fill="#8884d8"
+                        // padding angle is the gap between each pie slice
+                        paddingAngle={5}
+                        dataKey="value"
+                        isAnimationActive={true}
+                    />
+                    <Tooltip></Tooltip>
+                </PieChart>
+            </div>
         </div>
     );
 };
