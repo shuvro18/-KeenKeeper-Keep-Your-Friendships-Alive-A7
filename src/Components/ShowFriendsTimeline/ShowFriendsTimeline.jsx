74166a7492d4ea4ae08cf7d918389baed8b96330
shuvro18 <shuvrobiswas404@gmail.com>
2026-04-17 14:50:18 +0600

    timeline page are now okay

--- src/Components/ShowFriendsTimeline/ShowFriendsTimeline.jsx
+++ src/Components/ShowFriendsTimeline/ShowFriendsTimeline.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import call from "../../assets/call.png"
+import text from "../../assets/text.png"
+import video from "../../assets/video.png"
 
-const ShowFriendsTimeline = () => {
+const ShowFriendsTimeline = ({element}) => {
+    
+    
     return (
         <div>
-            
+            <div className='bg-white flex items-center gap-8 p-8'>
+                <img src={element.action === "call" ? `${call}`: element.action === "text" ? `${text}` : `${video}` } />
+                <div className='space-y-3'>
+                    <p>
+                        <span className='text-xl font-bold text-neutral/70'>Meetup </span>
+                        <span className='text-sm text-neutral/50'> With {element.name}</span>
+                    </p>
+                    <h3 className='text-neutral/50 '>{element.next_due_date}</h3>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Timeline/Timeline.jsx
+++ src/Components/Timeline/Timeline.jsx
@@ -1,18 +1,34 @@
 import React, { useContext } from 'react';
 import { friendContext } from '../FriendContext/Context';
+import ShowFriendsTimeline from '../ShowFriendsTimeline/ShowFriendsTimeline';
 
 const Timeline = () => {
 
-    const {button}= useContext(friendContext);
-    
-    
-    
+    const { button } = useContext(friendContext);
+
+
+
 
     return (
-        <div>
-            {
-                button.map((element,index)=>)
-            }
+        <div className='bg-base-200 p-15'>
+            <div className='w-9/12 mx-auto'>
+                <h2 className='font-bold text-3xl pt-9'>Timeline</h2>
+                <div className="py-10">
+                    <div className="dropdown  dropdown-start">
+                        <div tabIndex={0} role="button" className="btn m-1">Filter Timeline  ⬇️</div>
+                        <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                            <li onClick={()=>} ><a>Show All Calls</a></li>
+                            <li><a>Show All Text</a></li>
+                            <li><a>Show All Video(call)</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className='flex flex-col gap-5'>
+                    {
+                        button.map((element, index) => <ShowFriendsTimeline key={index} element={element}></ShowFriendsTimeline>)
+                    }
+                </div>
+            </div>
         </div>
     );
 };
